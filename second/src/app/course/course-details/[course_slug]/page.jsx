64c85f47b37d2ff88035c9bab4dba610da13bc51
Mvinit2245 <vinitmorankar22@gmail.com>
2026-04-04 14:34:45 +0530

    course details updated 3

--- second/src/app/course/course-details/[course_slug]/page.jsx
+++ second/src/app/course/course-details/[course_slug]/page.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
 
+import React from 'react';
 
-export default function page({ params }) {
+export default async function Page({ params }) {
+  const { course_slug } = await params;
+
   return (
     <>
-
     <main>
-      <h1> This is the course details Page</h1>
-    </main>
-    <main>
-      <h2>  <h1>You have selected the Course No {params.course_slug}</h1> </h2>
+      <h1>This is the course details Page</h1>
+      </main>
+      <main>
+      <h2>You have selected the Course No {course_slug}</h2>
     </main>
 
     </>
-  )
+  );
 }
